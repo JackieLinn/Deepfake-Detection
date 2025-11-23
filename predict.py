0f2632a16e74da92115c84b72f7f4c5176e9e3aa
JackieLinn <linxy20040304@163.com>
2025-11-23 12:08:01 +0800

    Add EfficientNetV2

--- predict.py
+++ predict.py
@@ -14,7 +14,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default='alexnet',
                         choices=['alexnet', 'googlenet', 'resnet', 'resnext', 'densenet', 'swint', 'mobilenet', 'mit',
-                                 'regnet', 'convnext', 'efficientnet'],
+                                 'regnet', 'convnext', 'efficientnet', 'ev2'],
                         help='model name')
     parser.add_argument('--num_classes', type=int, default=2)
     args = parser.parse_args()
